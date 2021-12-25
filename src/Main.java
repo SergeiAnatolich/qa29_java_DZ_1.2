b4bc6da9b758203f51c6180bd1c8b12fa0c40e9e
Sergey Scherbakov <sherbakov_s_a@mail.ru>
2021-12-25 16:43:13 +0300

    initial commit

---
+++ src/Main.java
@@ -0,0 +1,19 @@
+public class Main {
+    public static void main(String[] args) {
+        int score = 100; //счет пользователя
+        int refill = 1100; //пополнение счета
+        int multiplicity = 100; //кратность начисления бонусов
+        int bonusRate = 1; //бонусная ставка в рублях
+        int bonus;
+        if (refill > 100){
+            score = score+refill+refill/multiplicity;
+            bonus = refill/multiplicity;
+        } else {
+            score = score+refill;
+            bonus = 0;
+        }
+        System.out.println("Ваш баланс: " + score +
+                " рублей");
+        System.out.println("Ваш бонус: " + bonus + " рублей");
+    }
+}
